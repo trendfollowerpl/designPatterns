70cc179f89cc599ee57724b672b32ade786a3e7d
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-10-03 10:39:32 +0200

    bridge pattern

---
+++ Bridge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bridge Pattern");
+            Console.WriteLine(new Abstraction(new ImplementationA()).Operation());
+            Console.WriteLine(new Abstraction(new ImplementationB()).Operation());
+            Console.ReadLine();
+        }
+    }
+
+    interface Bridge
+    {
+        string OperationImp();
+    }
+
+    class Abstraction
+    {
+        private Bridge bridge;
+
+        public Abstraction(Bridge implementation)
+        {
+            bridge = implementation;
+        }
+
+        public string Operation()
+        {
+            return "Abstraction" + "<<<BRIDGE>>>" + bridge.OperationImp();
+        }
+    }
+
+    class ImplementationA : Bridge
+    {
+        public string OperationImp()
+        {
+            return "ImplementationA";
+        }
+    }
+
+    class ImplementationB : Bridge
+    {
+        public string OperationImp()
+        {
+            return "ImplementationB";
+        }
+    }
+}
