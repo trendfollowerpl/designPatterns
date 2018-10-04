22b21359590e5fb630c764fe824019d3ee8766d7
Piotr Szlaski <piotr.szlaski@ivanti.com>
2018-10-04 10:40:38 +0200

    Adapter pattern

---
+++ Adapter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new Adaptee();
+            Console.Write("Before the new standard\nPrecise reading: ");
+            Console.WriteLine(first.SpecificRequest(5, 3));
+            ITarget second = new Adapter();
+            Console.WriteLine("\nMoving to the new standard");
+            Console.WriteLine(second.Request(5));
+
+            Console.ReadLine();
+        }
+    }
+
+    class Adaptee
+    {
+        public double SpecificRequest(double a, double b)
+        {
+            return a / b;
+        }
+    }
+
+    interface ITarget
+    {
+        string Request(int i);
+    }
+
+    class Adapter : Adaptee, ITarget
+    {
+        public string Request(int i)
+        {
+            return "Rough estimate is " + (int)Math.Round(SpecificRequest(i, 3));
+        }
+    }
+}
